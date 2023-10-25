a29e02f6d203262fb1d743f86cf7a6b01a57743f
SkrimZz <97882900+SkrimZz@users.noreply.github.com>
2023-10-25 16:18:38 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,22 @@
+variable "gcp_project" {
+  description = "ID du projet Google Cloud"
+}
 
+variable "gcp_region" {
+  description = "Région Google Cloud pour le déploiement"
+  default     = "us-central1"
+}
+
+variable "machine_type" {
+  description = "Type de machine pour les instances VM"
+  default     = "n1-standard-1"
+}
+
+variable "mysql_password" {
+  description = "Mot de passe de la base de données MySQL/MariaDB"
+}
+
+variable "instance_count" {
+  description = "Nombre d'instances VM à créer"
+  default     = 2
+}
