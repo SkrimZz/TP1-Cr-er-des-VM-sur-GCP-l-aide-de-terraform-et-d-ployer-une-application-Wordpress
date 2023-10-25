fcb48a660d51f9ae32dcf2798c0c5dfe30c5e469
SkrimZz <97882900+SkrimZz@users.noreply.github.com>
2023-10-25 16:19:00 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
+provider "google" {
+  credentials = file("service-account-key.json")
+  project     = var.gcp_project
+  region      = var.gcp_region
+}
 
+# Création du VPC, sous-réseau, règles de pare-feu
+module "vpc" {
+  source = "./modules/vpc"
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+
+# Création des VM
+module "wordpress_vm" {
+  source = "./modules/wordpress_vm"
+  project        = var.gcp_project
+  region         = var.gcp_region
+  machine_type   = var.machine_type
+  mysql_password = var.mysql_password
+}
+
+module "db_vm" {
+  source = "./modules/db_vm"
+  project        = var.gcp_project
+  region         = var.gcp_region
+  machine_type   = var.machine_type
+  mysql_password = var.mysql_password
+  instance_count = var.instance_count
+}
